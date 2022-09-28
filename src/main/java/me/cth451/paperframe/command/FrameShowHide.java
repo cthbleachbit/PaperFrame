31d9b391bcedc615d0880cb810414bedba250fb3
Tianhao Chai <cth451@gmail.com>
2022-09-28 16:42:24 -0400

    FrameShowHide: use bukkit provided entity targeting

--- src/main/java/me/cth451/paperframe/command/FrameShowHide.java
+++ src/main/java/me/cth451/paperframe/command/FrameShowHide.java
@@ -1,19 +1,15 @@
 package me.cth451.paperframe.command;
 
-import com.destroystokyo.paper.block.TargetBlockInfo;
 import me.cth451.paperframe.PaperFramePlugin;
 import me.cth451.paperframe.util.Drawing;
 import org.bukkit.Color;
 import org.bukkit.Particle;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
-import org.bukkit.util.BoundingBox;
-import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 public class FrameShowHide implements CommandExecutor {
@@ -22,36 +18,6 @@ public class FrameShowHide implements CommandExecutor {
 
 	public FrameShowHide(PaperFramePlugin plugin) {
 		this.plugin = plugin;
-	}
-
-	/**
-	 * Find ItemFrame by minimizing angle difference
-	 *
-	 * @param player player requesting this search
-	 * @return the ItemFrame cloest to the player crosshair, or null if nothing is in range
-	 */
-	private ItemFrame findFrameByAngle(@NotNull Player player) {
-		ItemFrame frame = null;
-		float bestAngle = 0.2f;
-
-		Vector playerLookDir = player.getEyeLocation().getDirection();
-		Vector playerEyeLoc = player.getEyeLocation().toVector();
-
-		for (Entity entity : player.getNearbyEntities(SELECTION_RANGE, SELECTION_RANGE, SELECTION_RANGE)) {
-			if (!(entity instanceof ItemFrame)) {
-				continue;
-			}
-			Vector frameLoc = entity.getLocation().toVector();
-			Vector playerEntityVec = frameLoc.subtract(playerEyeLoc);
-			float angle = playerLookDir.angle(playerEntityVec);
-
-			if (bestAngle > angle) {
-				frame = (ItemFrame) entity;
-				bestAngle = angle;
-			}
-		}
-
-		return frame;
 	}
 
 	private ItemFrame findFrameByTargetedEntity(@NotNull Player player) {
@@ -67,33 +33,6 @@ public class FrameShowHide implements CommandExecutor {
 		return frame;
 	}
 
-	private ItemFrame findFrameByTargetedBlock(@NotNull Player player) {
-		TargetBlockInfo targetedBlock = player.getTargetBlockInfo(SELECTION_RANGE, TargetBlockInfo.FluidMode.NEVER);
-		if (targetedBlock == null || targetedBlock.getBlock().isEmpty()) {
-			return null;
-		}
-		Block frontBlock = targetedBlock.getBlock().getRelative(targetedBlock.getBlockFace());
-		BoundingBox box = new BoundingBox(frontBlock.getX(), frontBlock.getY(), frontBlock.getZ(),
-		                                  frontBlock.getX() + 1, frontBlock.getY() + 1, frontBlock.getZ() + 1);
-
-
-		for (Entity entity : player.getNearbyEntities(SELECTION_RANGE, SELECTION_RANGE, SELECTION_RANGE)) {
-			if (!(entity instanceof ItemFrame frame)) {
-				continue;
-			}
-			if (box.contains(frame.getLocation().toVector()) && frame.getAttachedFace()
-			                                                         .getOppositeFace() == targetedBlock.getBlockFace()) {
-				return frame;
-			}
-		}
-
-		// If no frames found...
-		Runnable drawCall = () -> Drawing.drawBoundingBox(box, player.getWorld(),
-		                                                  new Particle.DustOptions(Color.WHITE, 1.0f));
-		Drawing.scheduleStickyDraw(this.plugin, drawCall, 5, 10);
-		return null;
-	}
-
 	@Override
 	public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
 		if (!(commandSender instanceof Player player)) {
@@ -102,11 +41,11 @@ public class FrameShowHide implements CommandExecutor {
 		}
 
 		// Check whether the player is looking at an item frame
-		ItemFrame frame = findFrameByTargetedBlock(player);
+		ItemFrame frame = findFrameByTargetedEntity(player);
 
 		if (frame == null) {
 			// No item frames in range
-			player.sendMessage("Can't find an item frame on the block you are looking at");
+			player.sendMessage("Can't find an item frame where you are looking at");
 		} else {
 			boolean isVisible = frame.isVisible();
 			frame.setVisible(!isVisible);
